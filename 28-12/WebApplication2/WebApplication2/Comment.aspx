d7121260cc51db384a3e9543b664317d7d01d6f0
unknown <amer.m.amora@gmail.com>
2022-12-28 18:22:10 +0300

    28-12Task

---
+++ 28-12/WebApplication2/WebApplication2/Comment.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Comment.aspx.cs" Inherits="WebApplication2.Comment" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+        <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+            <ContentTemplate>
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+                <asp:TextBox ID="TextBox1" runat="server">
+
+                </asp:TextBox> <asp:Button ID="Button1" runat="server" Text="Button" OnClick="send" />
+                </ContentTemplate>
+            </asp:UpdatePanel>
+    </form>
+</body>
+</html>
---
+++ 28-12/WebApplication2/WebApplication2/Comment.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication2
+{
+    public partial class Comment : System.Web.UI.Page
+    {
+        SqlConnection connection = new SqlConnection("data source = DESKTOP-8NTQ6AN\\SQLEXPRESS; database = task2 ; integrated security=SSPI");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            print();
+         
+        }
+        protected void print() 
+        
+        {
+
+            connection.Open();
+            string table =
+"<table class='table table-striped'><tr><th>Comment</th></tr>";
+            SqlCommand comand = new SqlCommand("select comment from comment", connection);
+            SqlDataReader sdr = comand.ExecuteReader();
+            while (sdr.Read())
+            {
+                table += $"<tr><td>{sdr[0]}</td></tr>";
+            }
+            table += "</table>";
+            Label1.Text = table;
+            connection.Close();
+        }
+        protected void send(object sender, EventArgs e)
+        {
+            connection.Open();
+            SqlCommand comand = new SqlCommand($"insert into comment values('{TextBox1.Text}');", connection);
+            comand.ExecuteNonQuery();   
+            connection.Close();
+            print();
+
+        }
+    }
+}
