924030e91e9170b3c10113f151a25bcb2c9b641e
unknown <amer.m.amora@gmail.com>
2023-01-02 20:15:21 +0300

    Upload

---
+++ 3-1/Vote/Vote/WebUserControl1.ascx
@@ -0,0 +1,9 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="WebUserControl1.ascx.cs" Inherits="Vote.WebUserControl1" %>
+<asp:Label ID="question" runat="server"> How satisfied are you with our services</asp:Label>
+   <asp:RadioButton ID="RadioButton1" runat="server"  Text="Excellent" GroupName="vote" />  
+   <asp:RadioButton ID="RadioButton2" runat="server" Text="Very Good" GroupName="vote" />  
+   <asp:RadioButton ID="RadioButton3" runat="server" Text="Good" GroupName="vote" />  
+   <asp:RadioButton ID="RadioButton4" runat="server" Text="Bad" GroupName="vote" />  
+<asp:Label ID="label1" runat="server" Visible="false">Your Answer is : </asp:Label>
+<asp:Button ID="Button1" runat="server" Text="Button" OnClick="radioCheck" />
+    
---
+++ 3-1/Vote/Vote/WebUserControl1.ascx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Vote
+{
+    public partial class WebUserControl1 : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.Cookies["Vote"] != null)
+            {
+                label1.Visible = true;
+
+                question.Visible = false;
+                RadioButton1.Visible = false;
+                RadioButton2.Visible = false;
+                RadioButton3.Visible = false;
+                RadioButton4.Visible = false;
+                Button1.Visible = false;
+                label1.Text = "Your Answer is : "+Request.Cookies["Vote"].Value;
+            }
+
+        }
+        protected void radioCheck(object sender, EventArgs e)
+        {
+            string answer = "";
+            if (RadioButton1.Checked)
+            {
+                answer = RadioButton1.Text;
+
+            }
+            else if (RadioButton2.Checked)
+            {
+                answer = RadioButton2.Text;
+
+            }
+            else if (RadioButton3.Checked)
+            {
+                answer = RadioButton3.Text;
+
+            }
+            else if (RadioButton4.Checked)
+            {
+                answer = RadioButton4.Text;
+
+            }
+            Response.Cookies["Vote"].Value = answer;
+            Response.Cookies["Vote"].Expires= DateTime.Now.AddSeconds(20);
+            question.Visible = false;
+            RadioButton1.Visible= false;
+            RadioButton2.Visible= false;
+            RadioButton3.Visible= false;
+            RadioButton4.Visible = false;
+            Button1.Visible = false;
+            label1.Visible = true;
+            label1.Text += answer;
+        }
+    }
+}
